0ae50128c92093c349de3ef9a87d53cdfc1d4a95
yilmazderin <dyilmaz2@uwo.ca>
2024-01-17 17:11:16 -0500

    Add pages

--- for-emma/src/book/Book.jsx
+++ for-emma/src/book/Book.jsx
@@ -4,13 +4,67 @@ import "./book.css";
 const Book = () => {
     return (
         <div>
-            <input type="checkbox" id="checkbox-cover" />
-            <div className="book">
-                <div className="cover">
+            <input type="checkbox" id="checkbox-cover"/>
+            <input type="checkbox" id="checkbox-page1"/>
+            <input type="checkbox" id="checkbox-page2"/>
+            <input type="checkbox" id="checkbox-page3"/>
+            <div class="book">
+                <div class="cover">
+                    <i class="fa-solid fa-heart"></i>
                     <label htmlFor="checkbox-cover"></label>
                 </div>
-                <div className="page"></div>
-                <div className="back-cover"></div>
+                <div class="page" id="page1">
+                    <div class="front-page">
+                        <div class="page-title">
+                            <h3>Prequel</h3>
+                        </div>
+                        <div class="page-content">
+
+                        </div>
+                        <label class="next" for="checkbox-page1"><i class="fas fa-chevron-right"/></label>
+                    </div>
+                    <div class="back-page">
+                        <div class="page-title"></div>
+                        <div class="page-content"></div>
+                        <label class="prev" for="checkbox-page1"><i class="fas fa-chevron-left"/></label>
+                    </div>
+                </div>
+
+                <div class="page" id="page2">
+                    <div class="front-page">
+                        <div class="page-title">
+                            <h3>Chapter 1</h3>
+                        </div>
+                        <div class="page-content">
+                            <p>Dadada</p>
+                        </div>
+                        <label class="next" for="checkbox-page2"><i class="fas fa-chevron-right"/></label>
+                    </div>
+                    <div class="back-page">
+                        <div class="page-title"></div>
+                        <div class="page-content"></div>
+                        <label class="prev" for="checkbox-page2"><i class="fas fa-chevron-left"/></label>
+                    </div>
+                </div>
+
+                <div class="page" id="page3">
+                    <div class="front-page">
+                        <div class="page-title">
+                            <h3>Chapter 2</h3>
+                        </div>
+                        <div class="page-content">
+                            <p>Dada</p>
+                        </div>
+                        <label class="next" for="checkbox-page3"><i class="fas fa-chevron-right"/></label>
+                    </div>
+                    <div class="back-page">
+                        <div class="page-title"></div>
+                        <div class="page-content"></div>
+                        <label class="prev" for="checkbox-page3"><i class="fas fa-chevron-left"/></label>
+                    </div>
+                </div>
+
+                <div class="back-cover"></div>
             </div>
         </div>
     );
